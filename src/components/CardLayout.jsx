449bffa3ab4209eac0e2ac2026d26c5377d49daf
Moumita Seema <moumitaseemaa@gmail.com>
2026-01-31 12:47:44 +0600

    updated

--- src/components/CardLayout.jsx
+++ src/components/CardLayout.jsx
@@ -10,34 +10,6 @@ const CardLayout = ({
 }) => {
   let { pathname } = useLocation();
   return (
-    // <div
-    //   className={`color-card p-2 sm:p-6 rounded-2xl flex sm:flex-row flex-col-reverse items-center sm:items-start gap-2 font-inter justify-between text-center sm:text-start`}
-    // >
-    //   <div className="flex flex-col flex-1 gap-1 sm:gap-2 ">
-    //     <div className="flex gap-3 ">
-    //       {preIcon && <div className="">{preIcon}</div>}
-    //       <p
-    //         className={`text-gray text-sm font-normal leading-5 ${preIcon || "h-10 sm:h-auto"}`}
-    //       >
-    //         {title}
-    //       </p>
-    //     </div>
-    //     <p className="text-lg sm:text-2xl leading-9 text-white "> {count}</p>
-    //     <span
-    //       className={`text-sm font-normal leading-5 ${
-    //         percent?.startsWith("+")
-    //           ? "text-success"
-    //           : percent?.startsWith("-")
-    //             ? "text-red"
-    //             : "text-gray"
-    //       }`}
-    //     >
-    //       {percent}
-    //     </span>
-    //   </div>
-    //   {posIcon && <div className="size-8">{posIcon}</div>}
-    // </div>
-
     <div
       className={`color-card-bg p-2 sm:p-6 rounded-2xl flex sm:flex-row flex-col-reverse items-center sm:items-start gap-2 font-inter justify-between text-center sm:text-start`}
     >
--- src/components/CommonLayout.jsx
+++ src/components/CommonLayout.jsx
@@ -4,26 +4,16 @@ import Sidebar from "./Sidebar";
 
 const CommonLayout = () => {
   return (
-    //     <div className="flex bg-[linear-gradient(118.53deg,#020618_-34.38%,#162456_54.59%,#0F172B_143.57%)]">
-    //   <Sidebar />
-    //   <div className="w-full">
-    //     <Header />
-    //     <div className="ml-6">
-    //       <Outlet />
-    //     </div>
-    //   </div>
-    // </div>
-        <div className="flex mx-auto w-full bg-[linear-gradient(118.53deg,#020618_-34.38%,#162456_54.59%,#0F172B_143.57%)]">
-            <Sidebar />
-            <div className=" w-full">
-                <Header />
-                <div className="md:ml-6">
-                    <Outlet />
-                </div>
-            </div>
+    <div className="flex mx-auto w-full bg-[linear-gradient(118.53deg,#020618_-34.38%,#162456_54.59%,#0F172B_143.57%)]">
+      <Sidebar />
+      <div className=" w-full">
+        <Header />
+        <div className="md:ml-6">
+          <Outlet />
         </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default CommonLayout
-
+export default CommonLayout;
